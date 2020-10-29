03a6656d3875111943c55ee1620133eda4b38c22
Ariel I <arielibarra39@gmail.com>
2020-10-29 04:44:45 +0000

    build user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  
+  get '/signup' do 
+    erb :signup 
+  end 
+  
+  post '/signup' do 
+    uers = User.new(params)
+     if user.save 
+       redirect '/breweries'
+     else
+       redirect '/signup'
+     end
+   end 
+  
 end 
